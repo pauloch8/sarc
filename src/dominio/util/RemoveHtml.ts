66213bcd3994515c66176db62921102dd9a4fc09
pauloh.pires <pauloh.pires@inss.gov.br>
2023-03-15 22:57:35 -0300

    docs: jsdoc das classes TextoModelo e relacionadas

--- src/dominio/util/RemoveHtml.ts
+++ src/dominio/util/RemoveHtml.ts
@@ -1,3 +1,10 @@
+/**
+ * Interface de um Removedor de HTML que consiga receber um texto com html e retornar seu texto sem as marcações HTML. Esta interface é uma porta, que é concretizada em algum adaptador de tecnologia específica na camada de Infraestrutura
+ */
 export interface RemoveHtml {
+    /**
+     * Recebe uma string de um texto em html e retorna uma string do texto sem as marcações html
+     * @param htmlString
+     */
     remover(htmlString: string): string;
 }
