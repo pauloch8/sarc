4faacb035cb5a8b873499582bfd3d172f6d33003
pauloh.pires <pauloh.pires@inss.gov.br>
2023-06-14 16:06:35 -0300

    fix: texto de um modelo do relatório insirido em linha

--- src/dominio/processamento/processador/texto/Texto.ts
+++ src/dominio/processamento/processador/texto/Texto.ts
@@ -7,7 +7,13 @@ export class Texto implements ITexto {
     constructor(private _categoria: string, private _texto: string) {}
 
     get texto() {
-        return this._texto;
+        const regex = /^(?:<p>)?(?<conteudo>[\s\S]+?)(?:<\/p>)?$/;
+        const match = regex.exec(this._texto);
+        if (!match?.groups) {
+            throw new Error('Erro ao processar o texto');
+        }
+        const retorno = match.groups.conteudo;
+        return retorno;
     }
 
     get categoria() {
